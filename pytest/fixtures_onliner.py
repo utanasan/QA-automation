e568411a84769db47ba801b8cf328ae31265bd90
utanasan <80106490+utanasan@users.noreply.github.com>
2021-06-13 23:16:16 +0300

    Update fixtures_onliner.py
    comment deletion

--- pytest/fixtures_onliner.py
+++ pytest/fixtures_onliner.py
@@ -2,14 +2,14 @@ import pytest
 from selenium import webdriver
 
 
-# фикстура для запуска браузера перед тестом и закрытия после теста
+
 @pytest.fixture()
 def browser():
-    # запуск Chrome при начале каждого теста (до yield)
+    
     driver = webdriver.Chrome()
-    # открытие страницы при начале каждого теста
+   
     page = driver.get('https://www.onliner.by/')
-    # передача драйвера для использования в тесте
+    
     yield driver
-    # закрытие браузера после теста (после yield)
+    
     driver.close()
